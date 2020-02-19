a27a3a8969e84a78f706acf37dcb524b0c178bbe
yurido <u.dorofeev@gmail.com>
2020-02-19 17:26:18 +0100

    list of exercises for sprint, data model is implemented

--- frontend/src/app/models/sprint-exercises.ts
+++ frontend/src/app/models/sprint-exercises.ts
@@ -1,11 +1,19 @@
 import {Exercise} from './exercise';
 
 export class SprintExercises {
-  date: Date;
-  exercises: Exercise[] = [];
+  private date: Date;
+  private exercises: Exercise[] = [];
 
   constructor(date: Date, exercises: Exercise[]) {
     this.date = date;
     this.exercises = exercises;
+  }
+
+  public getSprintDay(): Date {
+    return this.date;
+  }
+
+  public getExercises(): Exercise[] {
+    return this.exercises;
   }
 }
