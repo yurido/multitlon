61e109050d4027e3891819a5ebf411b10634bbc4
yurido <u.dorofeev@gmail.com>
2020-03-23 16:22:11 +0100

    alert component cleanup

--- frontend/src/app/alert/alert.component.ts
+++ frontend/src/app/alert/alert.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {ErrorService} from '../services/error.service';
+import {faExclamationTriangle} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-alert',
@@ -10,15 +11,22 @@ import {ErrorService} from '../services/error.service';
 export class AlertComponent implements OnInit {
   @Input() error: any;
   message: string;
+  alertClass: any;
+  faExclamationTriangle = faExclamationTriangle;
 
   constructor(private errorService: ErrorService) {
   }
 
   ngOnInit(): void {
+    this.alertClass = 'alert-danger';
   }
 
   getMessage(): string {
     return this.errorService.getErrorMessage(this.error);
+  }
+
+  close(): void {
+    this.error = undefined;
   }
 
 }
