847beaea4e9e2dac0db3c040d0416d51cccfdc38
yurido <u.dorofeev@gmail.com>
2020-07-08 11:28:39 +0200

    deploy version is added

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  MONTHS: [{id: 0, name: 'january'},
+    {id: 1, name: 'february'},
+    {id: 2, name: 'march'},
+    {id: 3, name: 'april'},
+    {id: 4, name: 'may'},
+    {id: 5, name: 'jun'},
+    {id: 6, name: 'july'},
+    {id: 7, name: 'august'},
+    {id: 8, name: 'september'},
+    {id: 9, name: 'october'},
+    {id: 10, name: 'november'},
+    {id: 11, name: 'december'}
+  ]
 };
